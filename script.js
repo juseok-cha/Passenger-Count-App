2256498a90816365d7ed21e98eb00523854acca2
doma_cha <62501120+money17@users.noreply.github.com>
2021-07-25 16:47:12 +0000

    Reassigning & Incrementing
    Mission:
    1. Create a variable, bonusPoints. Initialize it as 50. Increase it to 100
    2.  Decrease it down to 25, and then finally increase it to 70

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // document.getElementById("count-el").innerText = 5
 
-// let firstBatch = 5
-// let secondBatch = 7 
-// let count = firstBatch + secondBatch
-// console.log(count)
+// Create a variable, bonusPoints. Initialize it as 50. Increase it to 100
+// Decrease it down to 25, and then finally increase it to 70
 
-let myAge = 20
-let humanDogRatio = 7
-let myDogAge = myAge * humanDogRatio
-console.log(myDogAge)
+let bonusPoints = 50 
+bonusPoints = bonusPoints + 50
+bonusPoints = bonusPoints - 75
+bonusPoints = bonusPoints + 45
+
+console.log(bonusPoints)
