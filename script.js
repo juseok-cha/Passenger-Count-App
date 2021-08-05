d895b5a245df294696468d10e0058de8d3410ac1
doma_cha <62501120+money17@users.noreply.github.com>
2021-08-06 00:42:08 +0900

    Final Version of Passenger Count App
    It's finally done!

--- script.js
+++ script.js
@@ -2,11 +2,20 @@
 // Change the count-el in the HTML to reflect the new count
 
 // camelCase
+let saveEl = document.getElementById('save-el')
 let countEl = document.getElementById('count-el')
-console.log(countEl)
-
 let count = 0
+
 function increment() {
-  count = count + 1
+  count += 1
   countEl.innerText = count
+}
+
+
+// 1.Create a function, save(), which logs out the count when it's called.
+function save(){
+  let countStr = count + " - ";
+  saveEl.textContent += countStr
+  countEl.textContent = 0
+  count = 0
 }
